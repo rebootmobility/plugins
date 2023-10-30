ee113488e6cc46dfe6fb7b3f345cde4329e76485
Paul Staskiewicz <staskiewicz@evemo.de>
2023-10-30 14:49:11 +0100

    feat(datetimepicker):  Allow minute interval/Add native picker

--- packages/datetimepicker/angular/index.ts
+++ packages/datetimepicker/angular/index.ts
@@ -1,48 +1,18 @@
-import { NgModule } from "@angular/core";
-import { registerElement } from "@nativescript/angular";
-import { DatePickerField, TimePickerField, DateTimePickerFields } from "@nativescript/datetimepicker";
-import {
-  DatePickerFieldDirective,
-  TimePickerFieldDirective,
-  DateTimePickerFieldsDirective,
-} from "./nativescript-datetimepicker.directives";
-import {
-  DatePickerValueAccessor,
-  TimePickerValueAccessor,
-  DateTimePickersValueAccessor,
-} from "./nativescript-datetimepicker.accessors";
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { DatePickerField, TimePickerField, DateTimePickerFields } from '@rebootmobility/datetimepicker';
+import { DatePickerFieldDirective, TimePickerFieldDirective, DateTimePickerFieldsDirective } from './nativescript-datetimepicker.directives';
+import { DatePickerValueAccessor, TimePickerValueAccessor, DateTimePickersValueAccessor } from './nativescript-datetimepicker.accessors';
 
-export {
-  DatePickerFieldDirective,
-  TimePickerFieldDirective,
-  DateTimePickerFieldsDirective,
-} from "./nativescript-datetimepicker.directives";
-export {
-  DatePickerValueAccessor,
-  TimePickerValueAccessor,
-  DateTimePickersValueAccessor,
-} from "./nativescript-datetimepicker.accessors";
+export { DatePickerFieldDirective, TimePickerFieldDirective, DateTimePickerFieldsDirective } from './nativescript-datetimepicker.directives';
+export { DatePickerValueAccessor, TimePickerValueAccessor, DateTimePickersValueAccessor } from './nativescript-datetimepicker.accessors';
 
 @NgModule({
-  declarations: [
-    DatePickerFieldDirective,
-    TimePickerFieldDirective,
-    DateTimePickerFieldsDirective,
-    DatePickerValueAccessor,
-    TimePickerValueAccessor,
-    DateTimePickersValueAccessor,
-  ],
-  exports: [
-    DatePickerFieldDirective,
-    TimePickerFieldDirective,
-    DateTimePickerFieldsDirective,
-    DatePickerValueAccessor,
-    TimePickerValueAccessor,
-    DateTimePickersValueAccessor,
-  ],
+	declarations: [DatePickerFieldDirective, TimePickerFieldDirective, DateTimePickerFieldsDirective, DatePickerValueAccessor, TimePickerValueAccessor, DateTimePickersValueAccessor],
+	exports: [DatePickerFieldDirective, TimePickerFieldDirective, DateTimePickerFieldsDirective, DatePickerValueAccessor, TimePickerValueAccessor, DateTimePickersValueAccessor],
 })
 export class NativeScriptDateTimePickerModule {}
 
-registerElement("DatePickerField", () => DatePickerField);
-registerElement("TimePickerField", () => TimePickerField);
-registerElement("DateTimePickerFields", () => DateTimePickerFields);
+registerElement('DatePickerField', () => DatePickerField);
+registerElement('TimePickerField', () => TimePickerField);
+registerElement('DateTimePickerFields', () => DateTimePickerFields);
